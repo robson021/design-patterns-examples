4449d63068e2ee0d8432c7107f5c75aa94c3b8db
robert <nowak021@gmail.com>
2017-05-21 17:39:44 +0200

    add builder pattern

---
+++ src/test/java/robert/tests/ProductBuilderTest.java
@@ -0,0 +1,20 @@
+package robert.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import robert.builder.Product;
+
+public class ProductBuilderTest {
+
+	@Test
+	public void builderTest() {
+		Product product = Product.newBuilder()
+				.withParam1(1)
+				.withParam2(2)
+				.build();
+
+		Assert.assertEquals(1, product.getParam1());
+		Assert.assertEquals(2, product.getParam2());
+		Assert.assertEquals(-1, product.getParam3());
+	}
+}
